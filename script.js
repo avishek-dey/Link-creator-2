a60b1fd58ce025f21f72eb7ed59b44119c6ff222
avishek-dey <139633713+avishek-dey@users.noreply.github.com>
2023-07-22 04:41:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
 document.getElementById('linkForm').addEventListener('submit', async function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const taskLinkValue = document.getElementById('taskLink').value;
-  const khLinkValue = document.getElementById('khLink').value;
+    const taskLinkValue = document.getElementById('taskLink').value;
+    const khLinkValue = document.getElementById('khLink').value;
 
-  // Replace 'YOUR_BITLY_API_KEY' with your actual Bitly API key
-  const apiKey = '7b24f37bdd5afc778a5e9e6bc1c0cb6a14a5db86';
-  
-  try {
-    const taskShortenedLink = await shortenLink(taskLinkValue, apiKey);
-    const khShortenedLink = await shortenLink(khLinkValue, apiKey);
+    // Replace 'YOUR_API_KEY' with your actual API key from shorten.rest
+    const apiKey = 'c7e437e0-2819-11ee-a6d4-f9cb5d3ad300';
+    
+    try {
+        const taskShortenedLink = await shortenLink(taskLinkValue, apiKey);
+        const khShortenedLink = await shortenLink(khLinkValue, apiKey);
 
-    const taskResultElement = document.getElementById('taskResult');
-    const khResultElement = document.getElementById('khResult');
+        const taskResultElement = document.getElementById('taskResult');
+        const khResultElement = document.getElementById('khResult');
 
-    taskResultElement.innerHTML = `Task link - <a href="${taskShortenedLink}" target="_blank">${taskShortenedLink}</a>`;
-    khResultElement.innerHTML = `KH link - <a href="${khShortenedLink}" target="_blank">${khShortenedLink}</a>`;
+        taskResultElement.innerHTML = `Task link - <a href="${taskShortenedLink}" target="_blank">${taskShortenedLink}</a>`;
+        khResultElement.innerHTML = `KH link - <a href="${khShortenedLink}" target="_blank">${khShortenedLink}</a>`;
 
-    // Clear the input fields
-    document.getElementById('taskLink').value = '';
-    document.getElementById('khLink').value = '';
-} catch (error) {
-    console.error('Error occurred while shortening links:', error);
-}
+        // Clear the input fields
+        document.getElementById('taskLink').value = '';
+        document.getElementById('khLink').value = '';
+    } catch (error) {
+        console.error('Error occurred while shortening links:', error);
+    }
 });
 
 async function shortenLink(link, apiKey) {
-  const apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
+    const apiUrl = 'https://api.shorten.rest/shorten';
 
-  const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({
-          long_url: link,
-      }),
-  });
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'apikey': apiKey,
+        },
+        body: JSON.stringify({
+            url: link,
+        }),
+    });
 
-  const data = await response.json();
-  return data.link;
+    const data = await response.json();
+    return data.short;
+}
+
+
 }
 
 // ... Same as before ...
